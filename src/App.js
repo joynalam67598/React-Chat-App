6acfcce09b5557b1c8b73773ad3b6af13e9fb17d
joynalam67598 <59138477+joynalam67598@users.noreply.github.com>
2021-08-30 01:22:50 +0600

    implement user login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { ChatEngine } from 'react-chat-engine';
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
 const App = () => {
+
+    if(!localStorage.getItem('username')) return <LoginForm />
+
     return (
         <ChatEngine
             height="100vh"
             projectID="6d709f25-7262-4da0-a9d2-6a3176ddc073"
-            userName="Rakib"
-            userSecret="123123"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
             // customize middle field chat feed
             // Sepred all the porps comming from the chatengine 
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/> }
